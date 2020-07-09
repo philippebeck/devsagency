cd0da7ac3a8583af1aafa774b8d557cb6fd2c9c1
Philippe Beck <philippe@philippebeck.net>
2020-07-09 11:31:47 +0200

    Replace recaptcha public token

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var onloadCallback = function() {
     grecaptcha.render("recaptcha", {
-        "sitekey" : "6Ld_2eYUAAAAAPHLRqN5mOEqZJqNKq7RO01CcwIy"
+        "sitekey" : "6Lfvha4ZAAAAAMxCpAlPOBunhJDMsHcBZcGTgS5r"
     });
 };

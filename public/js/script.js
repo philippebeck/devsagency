31c9c1dc8b7e0fc6f330648f559678130e824c21
Philippe Beck <philippe@philippebeck.net>
2020-07-16 22:36:36 +0200

    Move recaptcha script to his own file

--- public/js/script.js
+++ public/js/script.js
@@ -3,9 +3,3 @@
 document.addEventListener("DOMContentLoaded", function() {
     Animadio.slider("5000");
 });
-
-var onloadCallback = function() {
-    grecaptcha.render("recaptcha", {
-        "sitekey" : "6Lfvha4ZAAAAAMxCpAlPOBunhJDMsHcBZcGTgS5r"
-    });
-};

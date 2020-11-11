5246f69b01a521604c2f14c5c8dafb99061df719
Philippe Beck <philippe@philippebeck.net>
2020-11-12 00:49:14 +0100

    Fix Db for last changes

--- config/db.sql
+++ config/db.sql
@@ -38,7 +38,7 @@ CREATE TABLE `Graduates`
 )
     ENGINE=INNODB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `Users`
+CREATE TABLE `Members`
 (
     `id`            TINYINT         UNSIGNED    PRIMARY KEY AUTO_INCREMENT,
     `name`          VARCHAR(30)     NOT NULL    UNIQUE,
@@ -59,14 +59,15 @@ VALUES
 ('SEO Expertise',           'chart-line',           'Else if you have your own website & need Search Engine Optimization, we offer Structured Data creation & Programming Languages optimization.'),
 ('Programming Tool',        'robot',                'Even if your need is a Programming Tool for Website Building, we can offer a Completely Personalized Framework for your Own Projects.'),
 ('Digital Transformation',  'magic',                'But if you think that your Company need a real Digital Transformation, we can help you to transform old processes to Digital Processes & to optimize Time & Money.'),
-('Development Mentorship',  'chalkboard-teacher',   'Finally, we propose Development Mentorship for some kind of computer domains, like Frontend & Backend Development or else System Administration.');
+('Development Mentorship',  'chalkboard-teacher',   'Furthermore, we propose Development Mentorship for some kind of computer domains, like Frontend & Backend Development or else System Administration.'),
+('Developer Recruitments',  'user-plus',            'Finally, we offer the recruitment of Frontend, Backend PHP or JavaScript FullStack developers graduates from OpenClassrooms Online University.');
 
 INSERT INTO `Projects`
 (`name`,            `image`,            `link`,                                     `year`, `category`, `description`)
 VALUES
-('Animadio',        'animadio.png',     'www.npmjs.com/package/animadio',           2019,   'tool',     'CSS Framework'),
-('Pam',             'pam.png',          'packagist.org/packages/philippebeck/pam',  2018,   'tool',     'Php Approachable Microframework'),
+('Animadio',        'animadio.png',     'www.npmjs.com/package/animadio',           2019,   'tool',     'Animadio CSS Framework'),
+('Pam',             'pam.png',          'packagist.org/packages/devsagency/pam',    2018,   'tool',     'Php Approachable Microframework'),
 ('Animadio.js',     'animadio-js.png',  'www.npmjs.com/package/animadio.js',        2020,   'tool',     'Animadio JS Library'),
-('Animadio.org',    'animadio-org.jpg', 'animadio.org',                             2019,   'website',  'CSS Framework Website & Documentation'),
+('Animadio.org',    'animadio-org.jpg', 'animadio.org',                             2019,   'website',  'Animadio Website & Documentation'),
 ('Pam.net',         'pam-net.jpg',      'pam.devsagency.net',                       2020,   'website',  'Pam Website & Documentation'),
 ('Astronomy',       'astronomy.jpg',    'astronomy.philippebeck.net',               2020,   'website',  'Astronomy Website');
